4605c84af16e748890759beac74e843454b920bd
OmairXcluesiv <omair@xcluesiv.com>
2017-02-03 11:10:32 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,9 +9,13 @@ require 'scraperwiki/simple_html_dom.php';
  $html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.XAmerican+Revolutionary+War&_nkw=American+Revolutionary+War&_sacat=0");
 //
 // // Find something on the page using css selectors
-$dom = new simple_html_dom();
-$dom->load($html);
-print_r($dom->find("h3[class='lvtitle'] a"));
+$max_loop = 5;
+
+for($i=0;$i<=$max_loop;i++){
+ $dom = new simple_html_dom();
+ $dom->load($html);
+ print_r($dom->find("h3[class='lvtitle'] a"));
+}
 //
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
