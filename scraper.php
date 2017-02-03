1a4f302306352ad20496db88ad317aa32df59f42
OmairXcluesiv <omair@xcluesiv.com>
2017-02-03 16:12:33 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -7,8 +7,8 @@ require 'scraperwiki/simple_html_dom.php';
  $html = scraperwiki::scrape("http://www.ebay.com/sch/i.html?_from=R40&_trksid=p2050601.m570.l1313.TR0.TRC0.H0.XAmerican+Revolutionary+War&_nkw=American+Revolutionary+War&_sacat=0");
 //
 
-$max_loop = 5;
-for($i=0;$i<=$max_loop;$i++){
+$max_loop = 100;
+for($i=100;$i<=$max_loop;$i--){
  $dom = new simple_html_dom();
  $dom->load($html);
 
