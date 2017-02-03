96a46657a91b77f1fb61685531425332cf400e67
OmairXcluesiv <omair@xcluesiv.com>
2017-02-03 15:26:36 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -19,6 +19,7 @@ for($i=0;$i<=$max_loop;$i++){
  $m = $dom->find("li.lvprice");
  //echo strip_tags($r[0]). "<br>";
  echo "Product Title :" . $r[$i] . "\n";
- echo "Product Price: " . $m[0] . "<br>";
+ //echo "Product Price: " . $m[0] . "<br>";
+ $content = preg_replace(array('"<a href(.*?)>"', '"</a>"'), array('',''), $m[$i]);
 }
 ?>
